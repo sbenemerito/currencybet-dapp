55a9dc3d394b7cadb2e7caa123124b4c73bf6f68
Sam Benemerito <benemeritosam@gmail.com>
2018-11-01 22:12:11 +0900

    add bet functionality in smart contract

--- contracts/CurrencyBet.sol
+++ contracts/CurrencyBet.sol
@@ -34,15 +34,38 @@ contract ERC1203 is ERC20 {
 contract Currency is ERC1203 {
     using SafeMath for uint256;
 
-    enum Class {
+    enum CurrencyClass {
         bronze, // 0.01 ETH
         silver, // 0.05 ETH | convertible to 5 bronze per silver
         gold // 0.1 ETH | convertible to 2 silver per gold
     }
 
-    uint256 private constant BASE_CLASS = uint256(Class.bronze);
+    struct Player {
+        uint256 amountBet;
+        uint8 numberSelected;
+    }
+
+    address private owner;
+    address[] public players;
+    uint8 public betCount;
+    uint8 public constant BET_COUNT_LIMIT = 10;
+    uint256 private constant BASE_CLASS = uint256(CurrencyClass.bronze);
+    uint256 public totalBet;
+
+    function() public payable {}
+
+    constructor() public {
+        owner = msg.sender;
+        betCount = 0;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
     mapping(uint256 => uint256) private _supplies;
+    mapping(address => Player) private _bets;
     mapping(address => mapping(uint256 => uint256)) private _balances;
     mapping(address => mapping(address => mapping(uint256 => uint256))) private _allowances;
 
@@ -123,25 +146,25 @@ contract Currency is ERC1203 {
     }
 
     function fullyDilutedTotalSupply() public view returns (uint256) {
-        uint256 _bronzeSupply = _supplies[uint256(Class.bronze)];
-        uint256 _silverSupply = _supplies[uint256(Class.silver)];
-        uint256 _goldSupply = _supplies[uint256(Class.gold)];
+        uint256 _bronzeSupply = _supplies[uint256(CurrencyClass.bronze)];
+        uint256 _silverSupply = _supplies[uint256(CurrencyClass.silver)];
+        uint256 _goldSupply = _supplies[uint256(CurrencyClass.gold)];
 
         return fullyDilute(_bronzeSupply, _silverSupply, _goldSupply);
     }
 
     function fullyDilutedBalanceOf(address _owner) public view returns (uint256) {
-        uint256 _bronzeBalance = _balances[_owner][uint256(Class.bronze)];
-        uint256 _silverBalance = _balances[_owner][uint256(Class.silver)];
-        uint256 _goldBalance = _balances[_owner][uint256(Class.gold)];
+        uint256 _bronzeBalance = _balances[_owner][uint256(CurrencyClass.bronze)];
+        uint256 _silverBalance = _balances[_owner][uint256(CurrencyClass.silver)];
+        uint256 _goldBalance = _balances[_owner][uint256(CurrencyClass.gold)];
 
         return fullyDilute(_bronzeBalance, _silverBalance, _goldBalance);
     }
 
     function fullyDilutedAllowance(address _owner, address _spender) public view returns (uint256) {
-        uint256 _bronzeAllowance = _allowances[_owner][_spender][uint256(Class.bronze)];
-        uint256 _silverAllowance = _allowances[_owner][_spender][uint256(Class.silver)];
-        uint256 _goldAllowance = _allowances[_owner][_spender][uint256(Class.gold)];
+        uint256 _bronzeAllowance = _allowances[_owner][_spender][uint256(CurrencyClass.bronze)];
+        uint256 _silverAllowance = _allowances[_owner][_spender][uint256(CurrencyClass.silver)];
+        uint256 _goldAllowance = _allowances[_owner][_spender][uint256(CurrencyClass.gold)];
 
         return fullyDilute(_bronzeAllowance, _silverAllowance, _goldAllowance);
     }
@@ -151,9 +174,9 @@ contract Currency is ERC1203 {
         require(_value <= _balances[msg.sender][_fromClass]);
 
         uint256 _convertedValue;
-        if (_fromClass == uint256(Class.gold) && _toClass == uint256(Class.silver)) {
+        if (_fromClass == uint256(CurrencyClass.gold) && _toClass == uint256(CurrencyClass.silver)) {
             _convertedValue = goldToSilver(_value);
-        } else if (_fromClass == uint256(Class.silver) && _toClass == uint256(Class.bronze)) {
+        } else if (_fromClass == uint256(CurrencyClass.silver) && _toClass == uint256(CurrencyClass.bronze)) {
             _convertedValue = silverToBronze(_value);
         } else {
             revert();
@@ -161,6 +184,8 @@ contract Currency is ERC1203 {
 
         _balances[msg.sender][_fromClass] = _balances[msg.sender][_fromClass].safeSub(_value);
         _balances[msg.sender][_toClass] = _balances[msg.sender][_toClass].safeAdd(_convertedValue);
+        _supplies[_fromClass] = _supplies[_fromClass].safeSub(_value);
+        _supplies[_toClass] = _supplies[_toClass].safeAdd(_convertedValue);
 
         emit Conversion(_fromClass, _toClass, _value);
         return true;
@@ -181,31 +206,14 @@ contract Currency is ERC1203 {
 
         return _bronzeValue.safeAdd(_bronzeDilution);
     }
-}
 
-contract Betting is Currency {
-    address private _owner;
-    address[] public players;
-    uint8 public betCount;
-    uint8 public constant BET_COUNT_LIMIT = 10;
-    uint8[] private _selectedNumbers;
-    uint256 public totalBet;
-    uint256 public minimumBet = 0.001 ether;
-
-    struct Player {
-        uint256 amountBet;
-        uint8 numberSelected;
-    }
-
-    mapping(address => Player) private _bets;
-
-    function() public payable {}
-
-    constructor(uint256 _minimumBet) public {
-        _owner = msg.sender;
-        betCount = 0;
-        if (_minimumBet > 0) {
-            minimumBet = _minimumBet;
+    // Betting functions
+    function withdrawBalance(uint256 _amount) public onlyOwner {
+        require(_amount <= address(this).balance);
+        if (_amount > 0) {
+            owner.transfer(_amount);
+        } else {
+            owner.transfer(address(this).balance);
         }
     }
 
@@ -215,7 +223,6 @@ contract Betting is Currency {
 
     function _resetData() private {
         players.length = 0;
-        _selectedNumbers.length = 0;
         totalBet = 0;
         betCount = 0;
     }
@@ -244,27 +251,64 @@ contract Betting is Currency {
             delete _bets[playerAddress];
         }
 
-        uint256 winningAmount = totalBet / winnerCounter;
+        if (winnerCounter > 0) {
+            uint256 winningAmount = totalBet / winnerCounter;
 
-        for (uint8 y = 0; y < winners.length; y++) {
-            if (winners[y] != address(0)) winners[y].transfer(winningAmount);
+            for (uint8 y = 0; y < winners.length; y++) {
+                if (winners[y] != address(0)) {
+                    _balances[winners[y]][0] = _balances[winners[y]][0].safeAdd(winningAmount);
+                }
+            }
+        } else {
+            _supplies[0] = _supplies[0].safeSub(totalBet);
         }
 
         _resetData();
     }
 
-    function bet(uint8 _number) public payable {
-        require(!playerAlreadyBet(msg.sender)); // player cannot bet on the same category twice
-        require(msg.value >= minimumBet);
+    function bet(uint8 _number, uint256 _value, uint256 _betClass) public {
+        require(!playerAlreadyBet(msg.sender));  // player cannot bet on the same category twice
+        require(_value > 0);
+        require(_betClass >= 0 && _betClass <= 2);  // Currency class can only be 0-2
         require(_number >= 1 && _number <= 5);  // only numbers 1-5 can be numberSelected
 
-        _bets[msg.sender] = Player(msg.value, _number);
-        players.push(msg.sender);
-        totalBet = totalBet.safeAdd(msg.value);
-        betCount++;
-        _selectedNumbers.push(_number);
+        uint256 valueInBronze = _value;
+        if (_betClass != uint256(CurrencyClass.bronze)) {
+            require(convert(_betClass, uint256(CurrencyClass.bronze), _value));
 
-        if (betCount == BET_COUNT_LIMIT && _selectedNumbers.length > 1) _rewardWinners();
+            if (_betClass == uint256(CurrencyClass.gold)) {
+                valueInBronze = silverToBronze(goldToSilver(_value));
+            } else if (_betClass == uint256(CurrencyClass.silver)) {
+                valueInBronze = silverToBronze(_value);
+            } else {
+                revert();
+            }
+        }
+
+        _balances[msg.sender][0] = _balances[msg.sender][0].safeSub(valueInBronze);
+        _bets[msg.sender] = Player(valueInBronze, _number);
+        players.push(msg.sender);
+        totalBet = totalBet.safeAdd(valueInBronze);
+        betCount++;
+
+        if (betCount == BET_COUNT_LIMIT) _rewardWinners();
+    }
+
+    function buyCoin(uint256 _value, uint256 _buyingClass) public payable {
+        require(msg.value > 0);
+        require(_value > 0);
+        require(_buyingClass >= 0 && _buyingClass <= 2);  // Currency class can only be 0-2
+
+        _balances[msg.sender][_buyingClass] = _balances[msg.sender][_buyingClass].safeAdd(_value);
+        _supplies[_buyingClass] = _supplies[_buyingClass].safeAdd(_value);
+    }
+
+    function cashOut(uint256 _value, uint256 _coinClass) public {
+        require(_value > 0);
+        require(_coinClass >= 0 && _coinClass <= 2);  // Currency class can only be 0-2
+
+        _balances[msg.sender][_coinClass] = _balances[msg.sender][_coinClass].safeAdd(_value);
+        _supplies[_coinClass] = _supplies[_coinClass].safeAdd(_value);
     }
 }
 
